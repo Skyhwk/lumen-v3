710dc5b404c9a9e80eb906a68faf8d88087e9040
Skyhwk <dedi@intilab.com>
2025-08-12 08:31:25 +0700

    service save file

---
+++ app/Services/SaveFileServices.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class SaveFileServices
+{
+    public function saveFile($folder, $filename, $file)
+    {
+        try {
+            file_put_contents(public_path($folder . '/' . $filename), file_get_contents($file));
+            return true;
+        } catch (\Throwable $th) {
+            dd($th);
+            return false;
+        }
+    }
+}
