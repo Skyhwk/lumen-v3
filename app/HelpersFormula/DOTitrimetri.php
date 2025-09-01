ce525fa59febed6357e84f6f5ff4b3e2c409b6ac
aliffadhil-intilab <aliffadhil@intilab.com>
2025-09-01 11:09:08 +0700

    update DO Titrimetri

--- app/HelpersFormula/DOTitrimetri.php
+++ app/HelpersFormula/DOTitrimetri.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\HelpersFormula;
 use Carbon\Carbon;
@@ -6,13 +6,18 @@ use Carbon\Carbon;
 class DOTitrimetri
 {
     public function index($data, $id_parameter, $mdl){
-		// (A/0.025)* B * FP)
+		if(isset($data->konsetrasi_titran)){
+            // (A/0.025)* B * FP)
+            $rumus = number_format(($data->konsetrasi_titran / 0.025) * $data->volume_titrasi * $data->fp, 4, ',', '');
+        }else{
+            // HP * FP
+            $rumus = number_format($data->hp * $data->fp, 2, ',', '');
+        }
 
-        $rumus = number_format(($data->konsetrasi_titran / 0.025) * $data->volume_titrasi * $data->fp, 4, ',', '');
 		if(!is_null($mdl) && $rumus < $mdl){
             $rumus = '<' . $mdl;
         }
-		
+
 		$data = [
 			'hasil' => $rumus,
 			'hasil_2' => '',
