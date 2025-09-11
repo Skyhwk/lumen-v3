a52014120d8c9b537d0562b8600926c54ef4a6fb
goni2409 <goni@intilab.com>
2025-09-11 14:24:59 +0700

    update rumus direct reading

--- app/HelpersFormula/DirectHCHO8JpmLK.php
+++ app/HelpersFormula/DirectHCHO8JpmLK.php
@@ -6,11 +6,13 @@ use Carbon\Carbon;
 
 class DirectHCHO8JpmLK {
     public function index($data, $id_parameter, $mdl) {
+        
         $measurements = [];
 
-        foreach ($data->records as $record) {
-            if ($record['pengukuran']) {
-                $data = json_decode($record['pengukuran'], true);
+        foreach ($data as $record) {
+            
+            if ($record->pengukuran) {
+                $data = json_decode($record->pengukuran, true);
                 if (is_array($data) && !empty($data)) {
                     $total = array_sum($data);
                     $count = count($data);
@@ -22,8 +24,16 @@ class DirectHCHO8JpmLK {
         $average = !empty($measurements) ?
             number_format(array_sum($measurements) / count($measurements), 3) : 0;
 
+        $hasil = number_format((24.45*$average)/30.03, 3);
+        
+        if (!is_null($mdl) && $hasil < $mdl) {
+            $hasil = "<$mdl";
+        }
+
         return [
-            'average' => $average
+            'average' => $average,
+            'hasil' => $hasil,
+            'satuan' => 'ppm'
         ];
     }
 }
