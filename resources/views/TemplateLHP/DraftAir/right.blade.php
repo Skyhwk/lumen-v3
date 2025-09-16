f2f34fa813ff46dd76ff8b70d036d9cf5dfb4ec0
Skywhk <dedi@intilab.com>
2025-09-16 13:50:44 +0700

    air

--- resources/views/TemplateLHP/DraftAir/right.blade.php
+++ resources/views/TemplateLHP/DraftAir/right.blade.php
@@ -75,7 +75,11 @@
                             }, $dataArray);
 
                             foreach ($result as $index => $item) {
-                                $methode_sampling .= '<span><span>' . ($index + 1) . '. ' . $item . '</span></span><br>';
+                                if (trim($item) == '-') {
+                                    $methode_sampling .= "<span><span>-</span></span><br>";
+                                } else {
+                                    $methode_sampling .= "<span><span>" . ($index + 1) . ". " . $item . "</span></span><br>";
+                                }
                             }
 
                             if ($header->status_sampling == 'SD') {
@@ -89,7 +93,7 @@
                     <tr>
                         <td class="custom5">Metode Sampling</td>
                         <td class="custom5">:</td>
-                        <td class="custom5">{{ $methode_sampling }}</td>
+                        <td class="custom5">{!! $methode_sampling !!}</td>
                     </tr>
                     <tr>
                         <td class="custom5">Keterangan</td>
