42528826cb9bc66ee25cdb11bff296dd991cc9cf
IT-PROG4\Firos <firos@intilab.com>
2025-09-22 11:58:14 +0700

    clear star from regulasi

--- resources/views/TemplateLHP/DraftKebisingan/right.blade.php
+++ resources/views/TemplateLHP/DraftKebisingan/right.blade.php
@@ -66,7 +66,7 @@
                     @foreach (json_decode($header->regulasi) as $y)
                             <table style="padding-top: 10px;" width="100%">
                                 <tr>
-                                    <td class="custom5" colspan="3"><strong>**{{ explode('-',$y)[1] }}</strong></td>
+                                    <td class="custom5" colspan="3"><strong>{{ explode('-',$y)[1] }}</strong></td>
                                 </tr>
                             </table>
                     @endforeach
