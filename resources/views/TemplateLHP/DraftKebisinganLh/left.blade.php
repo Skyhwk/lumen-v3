efc1ca3883cdc557e77053cb4b63a3296aff2108
IT-PROG4\Firos <firos@intilab.com>
2025-09-23 16:01:36 +0700

    update sizing pdf for custom table head

--- resources/views/TemplateLHP/DraftKebisinganLh/left.blade.php
+++ resources/views/TemplateLHP/DraftKebisinganLh/left.blade.php
@@ -7,11 +7,11 @@
         <table style="border-collapse: collapse; font-family: Arial, Helvetica, sans-serif;">
             <thead>
                 <tr>
-                    <th width="6%"  class="pd-5-solid-top-center">NO</th>
-                    <th width="30%"  class="pd-5-solid-top-center">LOKASI SAMPLING</th>
-                    <th width="10%" class="pd-5-solid-top-center">HASIL UJI</th>
-                    <th width="30%" class="pd-5-solid-top-center">TITIK KOORDINAT</th>
-                    <th width="24%" class="pd-5-solid-top-center">TANGGAL SAMPLING</th>
+                    <th width="6%"  class="custom">NO</th>
+                    <th width="30%"  class="custom">LOKASI SAMPLING</th>
+                    <th width="10%" class="custom">HASIL UJI</th>
+                    <th width="30%" class="custom">TITIK KOORDINAT</th>
+                    <th width="24%" class="custom">TANGGAL SAMPLING</th>
                 </tr>
             </thead>
             <tbody>
