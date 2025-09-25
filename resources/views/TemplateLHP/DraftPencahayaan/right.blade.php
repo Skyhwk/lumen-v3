8fae1c5e9ab5299be0d953681df7d3e642402a17
Rangga Myd <ranggaamyd@gmail.com>
2025-09-25 15:46:11 +0700

    template pencahayaan

--- resources/views/TemplateLHP/DraftPencahayaan/right.blade.php
+++ resources/views/TemplateLHP/DraftPencahayaan/right.blade.php
@@ -8,12 +8,14 @@
             <td>
                 <table style="border-collapse: collapse; text-align: center;" width="100%">
                     <tr>
-                        <td class="custom" width="120">No. LHP</td>
-                        <td class="custom" width="200">JENIS SAMPEL</td>
+                        <td class="custom" width="33%">No. LHP</td>
+                        <td class="custom" width="33%">JENIS SAMPEL</td>
+                        <td class="custom" width="33%">PARAMETER UJI</td>
                     </tr>
                     <tr>
                         <td class="custom">{{ $header->no_lhp }}</td>
-                        <td class="custom">{{ $header->sub_kategori }}</td>
+                        <td class="custom">Lingkungan Kerja</td>
+                        <td class="custom">Pencahayaan Lingkungan Kerja <sup style="font-size: 8px;"><u>a</u></sup></td>
                     </tr>
                 </table>
             </td>
