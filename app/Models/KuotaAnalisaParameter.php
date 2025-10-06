986553ac9b644a8c8f00f3a9602706f45e100bf4
aliffadhil-intilab <aliffadhil@intilab.com>
2025-10-06 10:51:21 +0700

    new : kuota analisa lab

---
+++ app/Models/KuotaAnalisaParameter.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use App\Models\Sector;
+
+class KuotaAnalisaParameter extends Sector
+{
+    protected $table = "kuota_analisa_parameter";
+
+    public $timestamps = false;
+}
