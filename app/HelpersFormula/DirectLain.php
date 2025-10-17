a7843022ff541ed8381c19a8578ee2a861295c07
goni2409 <goni@intilab.com>
2025-10-17 16:29:50 +0700

    update rumus direct lain

--- app/HelpersFormula/DirectLain.php
+++ app/HelpersFormula/DirectLain.php
@@ -35,37 +35,50 @@ class DirectLain {
                     $c3 = $totalNilai / $jumlahElemen;
                     $c2 = 0.0409 * $c3 * 28.01;
                     $c1 = $c2 * 1000;
-                    $c4 = $c3 * 1000;
-                    $c5 = $c3 * 10000;
+                    $c4 = round($c3 * 1000, 6);
+                    $c5 = round($c3 * 10000, 6);
                     $satuan = "ppm";
 
-                    $c1 = $c1 < 11.45 ? '<11.45' : round($c1, 2);
-                    $c3 = $c3 < 0.01 ? '<0.01' : round($c3, 2);
-                    $c2 = $c2 < 0.01145 ? '<0.01145' : round($c2, 5);
-
-                    if($row->parameter == "C O"){
-                        $c15 = $c3;
-                        $c16 = $c1;
-                        $c17 = $c2;
+                    // simpan hasil murni dulu
+                    if ($row->parameter == "C O") {
+                        $c15 = round($c3,2);
+                        $c16 = round($c1,2);
+                        $c17 = round($c2,5);
                     }
-                } 
+
+                    // setelah semua hitung selesai, baru cek batas bawah
+                    if ($c1 < 11.45) $c1 = '<11.45'; else $c1 = round($c1, 2);
+                    if ($c3 < 0.01) $c3 = '<0.01'; else $c3 = round($c3, 2);
+                    if ($c2 < 0.01145) $c2 = '<0.01145'; else $c2 = round($c2, 5);
+                }
+
                 else if (in_array($row->parameter, $h2co)) {
-                    $c2 = $totalNilai / $jumlahElemen;
-                    $c2 = $c2 < 1 ? '<1' : round($c2, 0);
-                    $c1 = $c2 * 1000;
-                    $c3 = ($c2 / 24.45) * 30.03;
+                    $c2 = round($totalNilai / $jumlahElemen, 6);
+                    $c1 = round($c2 * 1000, 6);
+                    $c3 = round(($c2 / 24.45) * 30.03, 6);
                     $satuan = "mg/m3";
-                } 
+
+                    // cek batas bawah di akhir
+                    if ($c2 < 1) $c2 = '<1'; else $c2 = round($c2, 6);
+                }
+
                 else if (in_array($row->parameter, $paramVoc)) {
                     $c2 = $totalNilai / $jumlahElemen;
-                    $c2 = $c2 < 0.001 ? '<0.001' : round($c2, 3);
-                    $c1 = $c2 * 1000;
+                    $c1 = round($c2 * 1000, 6);
                     $satuan = "mg/m3";
-                } 
+
+                    // cek batas bawah di akhir
+                    if ($c2 < 0.001) $c2 = '<0.001'; else $c2 = round($c2, 3);
+                }
+
                 else if (in_array($row->parameter, $paramO2)) {
-                    $c5 = round($totalNilai / $jumlahElemen, 2);
+                    $c5 = $totalNilai / $jumlahElemen;
                     $satuan = "%";
-                } 
+
+                    // cek batas bawah di akhir
+                    $c5 = round($c5, 2);
+                }
+
                 // else if (in_array($row->parameter, $paramCO2)) {
                 //     $c3 = round($totalNilai / $jumlahElemen, 2);
                 //     $satuan = "%";
