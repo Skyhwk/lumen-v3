d10da7b2e5ed4465a2e45926ab52f8b00f751552
goni2409 <goni@intilab.com>
2025-10-22 16:09:35 +0700

    rumus direct untuk fdl direct lain

--- app/HelpersFormula/DirectHCHO8JpmLK.php
+++ app/HelpersFormula/DirectHCHO8JpmLK.php
@@ -8,8 +8,8 @@ class DirectHCHO8JpmLK {
     public function index($data, $id_parameter, $mdl) {
         
         $measurements = [];
-        $pa = $data->pluck('suhu')->toArray();
-        $ta = $data->pluck('tekanan_udara')->toArray();
+        $ta = $data->pluck('suhu')->toArray();
+        $pa = $data->pluck('tekanan_udara')->toArray();
         foreach ($data as $record) {
             if ($record->pengukuran) {
                 $data = json_decode($record->pengukuran, true);
@@ -23,23 +23,18 @@ class DirectHCHO8JpmLK {
 
         $average = !empty($measurements) ?
             number_format(array_sum($measurements) / count($measurements), 3) : 0;
-        $suhu = !empty($pa) ? round(array_sum($pa) / count($pa), 1) : 0;
-        $tekanan_udara = !empty($ta) ? round(array_sum($ta) / count($ta), 1) : 0;
+        $tekanan_udara = !empty($pa) ? round(array_sum($pa) / count($pa), 1) : 0;
+        $suhu = !empty($ta) ? round(array_sum($ta) / count($ta), 1) : 0;
 
         $c1 = $c2 = $c3 = $c4 = $c5 = $c15 = $c16 = $c17 = NULL;
             
-        // $c2 = round($average, 4); // mg/m³ 
         $c2 = round($average * ($suhu / $tekanan_udara) * (298 / 760), 4); // mg/m³
-        $c1 = round($c2 * 1000, 4); // ug/Nm³ misalnya
-        $c3 = number_format((24.45 / $average) * 30.03, 3);
+        $c1 = round($c2 * 1000, 4); // ug/Nm³ 
+        $c3 = round(($c2 / 24.45) * 30.03, 4); // mg/m³
         $c15 = $c3; // BDS
         $c17 = round($average, 4); // mg/m³
-        $c16 = round($c17 * 1000, 4); // ug/Nm³ misalnya
-        // $c3 = number_format((24.45*$average)/30.03, 3);
-        
-        // if (!is_null($mdl) && $hasil < $mdl) {
-        //     $hasil = "<$mdl";
-        // }
+        $c16 = round($c17 * 1000, 4); // ug/m³ 
+        $satuan = $id_parameter == 255 ? "mg/m3" : "mg/Nm3";
 
         return [
             'c1'     => $c1,
@@ -50,7 +45,7 @@ class DirectHCHO8JpmLK {
             'c15'    => $c15,
             'c16'    => $c16,
             'c17'    => $c17,
-            'satuan' => 'mg/m3'
+            'satuan' => $satuan
         ];
     }
 }
