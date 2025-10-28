3a08a1351b8a1c07a14245db3f9315ce5d956ed1
haroldintilab <harold@intilab.com>
2025-10-28 10:28:19 +0700

    UPDATE REKAP HASIL

--- app/Http/Controllers/api/RekapHasilPengujianController.php
+++ app/Http/Controllers/api/RekapHasilPengujianController.php
@@ -19,10 +19,38 @@ class RekapHasilPengujianController extends Controller
 {
     public function index()
     {
-        $linkLhp = LinkLhp::with('token')->where('is_emailed', true)->latest()->get();
+        $linkLhp = LinkLhp::with([
+            'token',
+            'order' => function ($query) {
+                $query->select('id', 'no_order');
+            },
+            'order.orderDetail' => function ($query) {
+                $query->select('id', 'id_order_header', 'tanggal_terima', 'tanggal_sampling', 'periode');
+            }
+        ])
+            ->where('is_emailed', true)
+            ->latest()
+            ->get()
+            ->map(function ($item) {
+                if ($item->order && $item->order->orderDetail) {
+                    $details = $item->order->orderDetail;
+
+                    $item->order->order_detail = [
+                        'periode'          => $details->pluck('periode')->filter()->values(),
+                        'tanggal_sampling' => $details->pluck('tanggal_sampling')->filter()->values(),
+                        'tanggal_terima'   => $details->pluck('tanggal_terima')->filter()->values(),
+                    ];
+
+                    // hilangkan data orderDetail mentah biar gak dobel
+                    unset($item->order->orderDetail);
+                }
+
+                return $item;
+            });
 
         return Datatables::of($linkLhp)->make(true);
     }
+
 
     public function reject(Request $request)
     {
