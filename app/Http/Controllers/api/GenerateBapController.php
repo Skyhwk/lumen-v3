85024ce8cc16516bacad0b54da0b59354bf00233
Skyhwk <dedi@intilab.com>
2025-11-05 16:02:23 +0700

    lanjut bap

--- app/Http/Controllers/api/GenerateBapController.php
+++ app/Http/Controllers/api/GenerateBapController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Carbon\Carbon;
+
 use App\Models\DokumenBap;
+use App\Models\LinkLhp;
+use App\Models\QuotationKontrakH;
+use App\Models\QuotationNonKontrak;
+
 use Yajra\Datatables\Datatables;
 
 class GenerateBapController extends Controller
@@ -14,5 +20,43 @@ class GenerateBapController extends Controller
         $dokumenBap = DokumenBap::with('order');
 
         return Datatables::of($dokumenBap)->make(true);
+    }
+
+    public function getNoQt(Request $request)
+    {
+        $linkLhps = LinkLhp::where('is_completed', 1)
+            ->select('no_quotation', 'periode', 'no_order', 'nama_perusahaan')
+            ->orderBy('periode') // pastikan periodenya terurut
+            ->get();
+
+        $grouped = [];
+
+        foreach ($linkLhps as $row) {
+            $noQuotation = $row->no_quotation;
+            if (!isset($grouped[$noQuotation])) {
+                $grouped[$noQuotation] = [
+                    'no_quotation'     => $noQuotation,
+                    'no_order'         => $row->no_order,
+                    'nama_perusahaan'  => $row->nama_perusahaan,
+                    'periodes'         => [],
+                ];
+            }
+            
+            if (!is_null($row->periode)) {
+                $grouped[$noQuotation]['periodes'][] = $row->periode;
+            }
+        }
+
+        foreach ($grouped as &$g) {
+            sort($g['periodes']);
+        }
+
+        $data = array_values($grouped);
+
+
+        return response()->json([
+            'data' => $data,
+            'status' => 200
+        ], 200);
     }
 }
