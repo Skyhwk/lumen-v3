76691a78ec971937c15c4feee0ca18893670773b
Ardanrestun <ardanrn67@gmail.com>
2025-11-13 18:51:28 +0700

    add: draft & tqc isokinetik

--- app/Http/Controllers/api/TqcEmisiSumberTidakBergerakIsokinetikController.php
+++ app/Http/Controllers/api/TqcEmisiSumberTidakBergerakIsokinetikController.php
@@ -1,18 +1,18 @@
 <?php
-
 namespace App\Http\Controllers\api;
 
+use App\Helpers\HelperSatuan;
 use App\Http\Controllers\Controller;
 use App\Models\DataLapanganEmisiCerobong;
 use App\Models\EmisiCerobongHeader;
-use App\Models\MasterBakumutu;
 use App\Models\HistoryAppReject;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
-use Yajra\DataTables\DataTables;
+use App\Models\IsokinetikHeader;
+use App\Models\MasterBakumutu;
 use App\Models\OrderDetail;
 use Carbon\Carbon;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
 {
@@ -21,13 +21,17 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
         $data = OrderDetail::where('is_active', true)
             ->where('status', 1)
             ->where('kategori_2', '5-Emisi')
-            ->where('kategori_3',  '34-Emisi Sumber Tidak Bergerak')
+            ->whereIn('kategori_3', [
+                '34-Emisi Sumber Tidak Bergerak',
+                '119-Emisi Isokinetik',
+            ])
+            ->where('parameter', 'like', '%Iso-%')
             ->orderBy('id', 'desc');
 
         return DataTables::of($data)
             ->filter(function ($query) {
                 foreach (request('columns', []) as $col) {
-                    $name = $col['data'] ?? null;
+                    $name   = $col['data'] ?? null;
                     $search = $col['search']['value'] ?? null;
 
                     if ($search && in_array($name, [
@@ -47,23 +51,41 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
             ->make(true);
     }
 
-
     public function detail(Request $request)
     {
         try {
+
+            $parameter = OrderDetail::where('no_sampel', $request->no_sampel)
+                ->where('is_active', true)
+                ->first()->parameter;
+
+            $parameterArray = json_decode($parameter, true);
+
+            $parameterNames = array_map(function ($param) {
+                $parts = explode(';', $param);
+                return trim(end($parts));
+            }, $parameterArray);
+
             $cerobong = EmisiCerobongHeader::with(['ws_value_cerobong'])
                 ->where('no_sampel', $request->no_sampel)
                 ->where('is_approved', 1)
                 ->where('status', 0)
-                ->select('id', 'no_sampel', 'id_parameter', 'parameter', 'lhps', 'is_approved', 'approved_by', 'approved_at', 'created_by', 'created_at', 'status', 'is_active')
+                ->whereIn('parameter', $parameterNames)
                 ->get();
 
-
+            $isokinetik = IsokinetikHeader::with(['ws_value'])
+                ->where('no_sampel', $request->no_sampel)
+                ->where('is_approved', 1)
+                ->where('status', 0)
+                ->whereIn('parameter', $parameterNames)
+                ->get();
+            $data = $cerobong->merge($isokinetik);
 
             // $id_regulasi = explode("-", json_decode($request->regulasi)[0])[0];
             $id_regulasi = $request->regulasi;
-            foreach ($cerobong as $item) {
+            $getSatuan   = new HelperSatuan;
 
+            foreach ($data as $item) {
                 $dataLapangan = DataLapanganEmisiCerobong::where('no_sampel', $item->no_sampel)
                     ->select('waktu_pengambilan')
                     ->first();
@@ -72,24 +94,26 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
                     ->where('is_active', 1)
                     ->select('baku_mutu', 'satuan', 'method')
                     ->first();
-                $item->durasi = $dataLapangan->waktu_pengambilan ?? null;
-                $item->satuan = $bakuMutu->satuan ?? null;
-                $item->baku_mutu = $bakuMutu->baku_mutu ?? null;
-                $item->method = $bakuMutu->method ?? null;
+                $item->durasi      = $dataLapangan->waktu_pengambilan ?? null;
+                $item->satuan      = $bakuMutu->satuan ?? null;
+                $item->baku_mutu   = $bakuMutu->baku_mutu ?? null;
+                $item->method      = $bakuMutu->method ?? null;
                 $item->nama_header = $bakuMutu->nama_header ?? null;
 
-                $index = $this->kumpulanSatuan($item->satuan);
-                $ws = $item->ws_value_cerobong ?? null;
-                if (!$ws) return "noWs";
+                $index = $getSatuan->emisi($item->satuan);
+                $ws    = $item->ws_value_cerobong ?? $item->ws_value ?? null;
+                if (! $ws) {
+                    return "noWs";
+                }
 
-                $ws =  $ws->toArray();
+                $ws    = $ws->toArray();
                 $nilai = null;
 
                 if ($index === null) {
                     // Cari dari f_koreksi_c...f_koreksi_c10
                     for ($i = 0; $i <= 10; $i++) {
                         $key = $i === 0 ? 'f_koreksi_c' : "f_koreksi_c$i";
-                        if (!empty($ws[$key])) {
+                        if (! empty($ws[$key])) {
                             $nilai = $ws[$key];
                             break;
                         }
@@ -102,12 +126,30 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
 
                             // Khusus C3, kalau kosong ambil dari C3_persen
                             if ($i === 3) {
-                                $nilai = !empty($ws[$key]) ? $ws[$key] : ($ws['C3_persen'] ?? null);
-                            } elseif (!empty($ws[$key])) {
+                                $nilai = ! empty($ws[$key]) ? $ws[$key] : ($ws['C3_persen'] ?? null);
+                            } elseif (! empty($ws[$key])) {
                                 $nilai = $ws[$key];
                             }
 
-                            if (!empty($nilai)) break;
+                            if (! empty($nilai)) {
+                                break;
+                            }
+
+                        }
+                    }
+
+                    if (empty($nilai)) {
+                        $hasilIsokinetik = json_decode($ws['hasil_isokinetik'], true);
+
+                        if (is_array($hasilIsokinetik)) {
+                            $nilaiText = [];
+                            foreach ($hasilIsokinetik as $key => $value) {
+                                $label       = ucwords(str_replace('_', ' ', $key));
+                                $nilaiText[] = "{$label}: {$value}";
+                            }
+                            $nilai = implode(' | ', $nilaiText);
+                        } else {
+                            $nilai = $hasilIsokinetik;
                         }
                     }
 
@@ -119,54 +161,16 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
                     $nilai = $ws[$fKoreksiKey] ?? $ws[$hasilKey] ?? '-';
                 }
 
-
-
-
                 $item->nilai_uji = $nilai;
             }
 
-            return Datatables::of($cerobong)
+            return Datatables::of($data)
                 ->make(true);
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => $th->getMessage(),
             ], 401);
         }
-    }
-
-    private function kumpulanSatuan($satuan)
-    {
-        $satuanIndexMap = [
-            "μg/Nm³" => "",
-            "μg/Nm3" => "",
-
-            "mg/nm³" => 1,
-            "mg/nm3" => 1,
-            "mg/Mm³" => 1,
-            "mg/Nm3" => 1,
-            "mg/Nm³" => 1,
-            "mg/Nm³" => 1,
-
-            "ppm"    => 2,
-            "PPM" => 2,
-
-            "ug/m3" => 3,
-            "ug/m³" => 3,
-
-            "mg/m3"  => 4,
-            "mg/m³"  => 4,
-            "mg/m³"  => 4,
-
-            "%"      => 5,
-            "°C"     => 6,
-            "g/gmol" => 7,
-            "m3/s"   => 8,
-            "m/s"    => 9,
-            "kg/tahun" => 10,
-        ];
-
-
-        return $satuanIndexMap[$satuan] ?? null;
     }
 
     public function detailLapangan(Request $request)
@@ -183,7 +187,6 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
         }
     }
 
-
     public function handleApproveSelected(Request $request)
     {
         DB::beginTransaction();
@@ -197,14 +200,14 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
             return response()->json([
                 'message' => 'Data berhasil diapprove.',
                 'success' => true,
-                'status' => 200,
+                'status'  => 200,
             ], 200);
         } catch (\Throwable $th) {
             DB::rollBack();
             return response()->json([
                 'message' => 'Gagal mengapprove data: ' . $th->getMessage(),
                 'success' => false,
-                'status' => 500,
+                'status'  => 500,
             ], 500);
         }
     }
@@ -215,11 +218,10 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
             ->where('is_active', 1)
             ->get();
 
-
         $data = [];
         foreach ($orderDetails as $orderDetail) {
             $dataLapanganEmisiCerobong = DataLapanganEmisiCerobong::where('no_sampel', $orderDetail->no_sampel)->get();
-            $mapHasil = fn($col) => $col->values()
+            $mapHasil                  = fn($col) => $col->values()
                 ->map(fn($hasil_uji) => json_encode([
                     'co2' => $hasil_uji->co2,
                     'co'  => $hasil_uji->co,
@@ -229,34 +231,32 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
                 ->toArray();
 
             $currentDataLapangan = $dataLapanganEmisiCerobong->where('no_sampel', $orderDetail->no_sampel);
-            $hasil   = $mapHasil($currentDataLapangan);
-            $history = $mapHasil($dataLapanganEmisiCerobong->where('no_sampel', '!=', $orderDetail->no_sampel));
+            $hasil               = $mapHasil($currentDataLapangan);
+            $history             = $mapHasil($dataLapanganEmisiCerobong->where('no_sampel', '!=', $orderDetail->no_sampel));
 
             $currentDataLapangan = $currentDataLapangan->first();
 
             $data[] = [
-                'no_sampel' => $orderDetail->no_sampel,
-                'titik' => $orderDetail->keterangan_1,
-                'history' => $history,
-                'hasil' => $hasil,
-                'sampler' => $currentDataLapangan->created_by,
-                'approved_by' => $currentDataLapangan->approved_by,
+                'no_sampel'                     => $orderDetail->no_sampel,
+                'titik'                         => $orderDetail->keterangan_1,
+                'history'                       => $history,
+                'hasil'                         => $hasil,
+                'sampler'                       => $currentDataLapangan->created_by,
+                'approved_by'                   => $currentDataLapangan->approved_by,
 
-
-                'id' => $currentDataLapangan->id,
-                'nama_perusahaan' => $orderDetail->nama_perusahaan,
-                'no_order' => $orderDetail->no_order,
-                'kategori_3' => $orderDetail->kategori_3,
-                'data_lapangan_emisi_kendaraan' => $currentDataLapangan
+                'id'                            => $currentDataLapangan->id,
+                'nama_perusahaan'               => $orderDetail->nama_perusahaan,
+                'no_order'                      => $orderDetail->no_order,
+                'kategori_3'                    => $orderDetail->kategori_3,
+                'data_lapangan_emisi_kendaraan' => $currentDataLapangan,
             ];
         }
 
         return response()->json([
-            'data' => $data,
+            'data'    => $data,
             'message' => 'Data retrieved successfully',
         ], 200);
     }
-
 
     public function approveData(Request $request)
     {
@@ -267,26 +267,26 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
                 $data->status = 2;
                 $data->save();
                 HistoryAppReject::insert([
-                    'no_lhp' => $data->cfr,
-                    'no_sampel' => $data->no_sampel,
-                    'kategori_2' => $data->kategori_2,
-                    'kategori_3' => $data->kategori_3,
-                    'menu' => 'TQC Emisi Sumber Tidak Bergerak',
-                    'status' => 'approve',
+                    'no_lhp'      => $data->cfr,
+                    'no_sampel'   => $data->no_sampel,
+                    'kategori_2'  => $data->kategori_2,
+                    'kategori_3'  => $data->kategori_3,
+                    'menu'        => 'TQC Emisi Isokinetik',
+                    'status'      => 'approve',
                     'approved_at' => Carbon::now(),
-                    'approved_by' => $this->karyawan
+                    'approved_by' => $this->karyawan,
                 ]);
                 DB::commit();
                 return response()->json([
-                    'status' => 'success',
-                    'message' => 'Data tqc no sample ' . $data->no_sampel . ' berhasil diapprove'
+                    'status'  => 'success',
+                    'message' => 'Data tqc no sample ' . $data->no_sampel . ' berhasil diapprove',
                 ]);
             }
         } catch (\Throwable $th) {
             DB::rollBack();
             return response()->json([
-                'status' => 'error',
-                'message' => 'Terjadi kesalahan ' . $th->getMessage()
+                'status'  => 'error',
+                'message' => 'Terjadi kesalahan ' . $th->getMessage(),
             ]);
         }
     }
@@ -301,15 +301,15 @@ class TqcEmisiSumberTidakBergerakIsokinetikController extends Controller
                 $data->save();
                 DB::commit();
                 return response()->json([
-                    'status' => 'success',
-                    'message' => 'Data tqc no sample ' . $data->no_sampel . ' berhasil direject'
+                    'status'  => 'success',
+                    'message' => 'Data tqc no sample ' . $data->no_sampel . ' berhasil direject',
                 ]);
             }
         } catch (\Throwable $th) {
             DB::rollBack();
             return response()->json([
-                'status' => 'error',
-                'message' => 'Terjadi kesalahan ' . $th->getMessage()
+                'status'  => 'error',
+                'message' => 'Terjadi kesalahan ' . $th->getMessage(),
             ]);
         }
     }
