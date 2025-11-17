03ae6c794f118ed14c3b64bffe0b189d55f2941b
Skyhwk <dedi@intilab.com>
2025-11-17 17:47:22 +0700

    approve auto email kebisingan

--- app/Http/Controllers/api/DraftUdaraKebisinganController.php
+++ app/Http/Controllers/api/DraftUdaraKebisinganController.php
@@ -8,12 +8,14 @@ use App\Models\LhpsKebisinganHeader;
 use App\Models\LhpsKebisinganDetail;
 use App\Models\LhpsKebisinganCustom;
 
+use App\Helpers\EmailLhpRilisHelpers;
 
 use App\Models\LhpsKebisinganHeaderHistory;
 use App\Models\LhpsKebisinganDetailHistory;
 
 
 use App\Models\MasterSubKategori;
+use App\Models\OrderHeader;
 use App\Models\OrderDetail;
 use App\Models\MasterRegulasi;
 use App\Models\MasterKaryawan;
@@ -606,9 +608,11 @@ class DraftUdaraKebisinganController extends Controller
 
                 $konfirmasiLhp->save();
             }
+
             $data = LhpsKebisinganHeader::where('no_lhp', $request->cfr)
                 ->where('is_active', true)
                 ->first();
+
             $noSampel = array_map('trim', explode(',', $request->noSampel));
             $no_lhp = $data->no_lhp;
 
@@ -636,11 +640,6 @@ class DraftUdaraKebisinganController extends Controller
                 $data->is_approve = 1;
                 $data->approved_at = Carbon::now()->format('Y-m-d H:i:s');
                 $data->approved_by = $this->karyawan;
-                // if ($data->count_print < 1) {
-                //     $data->is_printed = 1;
-                //     $data->count_print = $data->count_print + 1;
-                // }
-                // dd($data->id_kategori_2);
 
                 HistoryAppReject::insert([
                     'no_lhp' => $data->no_lhp,
@@ -662,21 +661,26 @@ class DraftUdaraKebisinganController extends Controller
                     $qr->save();
                 }
 
-                // $servicePrint = new PrintLhp();
-                // $servicePrint->printByFilename($data->file_lhp, $detail);
-
-                // if (!$servicePrint) {
-                //     DB::rollBack();
-                //     return response()->json(['message' => 'Gagal Melakukan Reprint Data', 'status' => '401'], 401);
-                // }
-
-                $periode = OrderDetail::where('cfr', $data->no_lhp)->where('is_active', true)->first()->periode ?? null;
-                $cekLink = LinkLhp::where('no_order', $data->no_order)->where('periode', $periode)->first();
+                $cekDetail = OrderDetail::where('cfr', $data->no_lhp)->where('is_active', true)->first();
+                $cekLink = LinkLhp::where('no_order', $data->no_order)->where('periode', $cekDetail->periode)->first();
 
                 if($cekLink) {
-                    $job = new CombineLHPJob($data->no_lhp, $data->file_lhp, $data->no_order, $this->karyawan, $periode);
+                    $job = new CombineLHPJob($data->no_lhp, $data->file_lhp, $data->no_order, $this->karyawan, $cekDetail->periode);
                     $this->dispatch($job);
                 }
+
+                $orderHeader = OrderHeader::where('id', $cekDetail->id_order_header)
+                ->first();
+
+                EmailLhpRilisHelpers::run([
+                    'cfr'              => $request->cfr,
+                    'no_order'         => $data->no_order,
+                    'nama_pic_order'   => $orderHeader->nama_pic_order ?? '-',
+                    'nama_perusahaan'  => $data->nama_pelanggan,
+                    'periode'          => $cekDetail->periode,
+                    'karyawan'         => $this->karyawan
+                ]);
+
             } else {
                 DB::rollBack();
                 return response()->json([
