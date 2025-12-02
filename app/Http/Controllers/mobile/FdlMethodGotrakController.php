0f9e61c2949747236402d07de7a583e7054ee300
goni2409 <goni@intilab.com>
2025-12-02 15:48:38 +0700

    ergonomi

--- app/Http/Controllers/mobile/FdlMethodGotrakController.php
+++ app/Http/Controllers/mobile/FdlMethodGotrakController.php
@@ -165,7 +165,7 @@ class FdlMethodGotrakController extends Controller
             if ($request->aktivitas != '')
                 $data->aktivitas = $request->aktivitas;
             $data->method = 7;
-            $data->pengukuran = json_encode($pengukuran);
+            $data->pengukuran = json_encode($pengukuran, JSON_UNESCAPED_UNICODE);
             if ($request->foto_samping_kiri != '')
                 $data->foto_samping_kiri = self::convertImg($request->foto_samping_kiri, 1, $this->user_id);
             if ($request->foto_samping_kanan != '')
