d7dd2cf4b62d63d8bde9f172fb350eeb7b8519b2
Skyhwk <dedi@intilab.com>
2025-12-25 16:03:21 +0700

    tambah loging

--- app/Console/Commands/SchaduleUpdateQsd.php
+++ app/Console/Commands/SchaduleUpdateQsd.php
@@ -27,13 +27,13 @@ class SchaduleUpdateQsd extends Command
     public function handle()
     {
         try {
-            // printf("[SchaduleUpdateQsd] [%s] Start Running...", date('Y-m-d H:i:s'));
+            printf("[SchaduleUpdateQsd] [%s] Start Running...", date('Y-m-d H:i:s'));
             SalesDailyQSD::run();
-            // printf("\n[SchaduleUpdateQsd] [%s] Running done, delay 3 detik sebelum hitung kpi", date('Y-m-d H:i:s'));
+            printf("\n[SchaduleUpdateQsd] [%s] Running done, delay 3 detik sebelum hitung kpi", date('Y-m-d H:i:s'));
             sleep(3);
-            // printf("\n[SchaduleUpdateQsd] [%s] Hitung kpi done", date('Y-m-d H:i:s'));
+            printf("\n[SchaduleUpdateQsd] [%s] Start Running Kpi", date('Y-m-d H:i:s'));
             SalesKpiMonthly::run();
-            // printf("\n[SchaduleUpdateQsd] [%s] Running done", date('Y-m-d H:i:s'));
+            printf("\n[SchaduleUpdateQsd] [%s] Running Kpi done", date('Y-m-d H:i:s'));
         } catch (\Throwable $th) {
             Log::error('[WorkerDailyQSDSales] Error: ' . $th->getMessage());
         }
