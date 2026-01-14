4c7ac7d5dd99a31294923d47ae89f0e097adfcda
Ardanrestun <ardanrn67@gmail.com>
2026-01-14 10:38:48 +0700

    add:  $NotReAssign[] = $customer;

--- app/Services/RandomSalesAssign.php
+++ app/Services/RandomSalesAssign.php
@@ -198,6 +198,7 @@ class RandomSalesAssign
                             // } else {
                             //     $NotReAssign[] = $customer;
                             // }
+                            $NotReAssign[] = $customer;
 
                             continue;
                         }
