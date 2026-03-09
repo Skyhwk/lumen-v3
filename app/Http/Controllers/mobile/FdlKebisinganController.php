e703cf33e19ca895d35e617e91e55c37e7a57c1f
goni2409 <goni@intilab.com>
2026-03-09 15:16:21 +0700

    kebisingan min max range

--- app/Http/Controllers/mobile/FdlKebisinganController.php
+++ app/Http/Controllers/mobile/FdlKebisinganController.php
@@ -74,7 +74,9 @@ class FdlKebisinganController extends Controller
                 'id_ket'             => explode('-', $order->kategori_3)[0],
                 'id_ket2'            => explode('-', $order->kategori_2)[0],
                 'param'              => $order->parameter,
-                'kategori_pengujian' => $waktu
+                'kategori_pengujian' => $waktu,
+                'min_range'          => floatval(30.0),
+                'max_range'          => floatval(120.0),
             ], 200);
         }
 
