057aaa3b40eb0a96ad27eea42166979f7a73f6cc
Ardanrestun <ardanrn67@gmail.com>
2026-03-13 15:40:45 +0700

    adjust: other air

--- app/Http/Controllers/api/OthersController.php
+++ app/Http/Controllers/api/OthersController.php
@@ -29,14 +29,7 @@ class OthersController extends Controller
             ->where('subkontrak.is_active', true)
             ->where('subkontrak.is_total', false)
             ->orderBy('subkontrak.created_at', 'desc')
-            ->select('subkontrak.*')
-             ->orderByRaw("
-                CASE 
-                    WHEN tanggal_terima IS NULL THEN 1
-                    ELSE 0
-                END,
-                tanggal_terima DESC
-            ");
+            ->select('subkontrak.*');
         return Datatables::of($data)
             ->addColumn('tanggal_terima', function ($item) {
                 return $item->order_detail->tanggal_terima ?? '-';
