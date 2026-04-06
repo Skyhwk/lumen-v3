6a031622da3527e7488d75b948eae76bb9a4e7b9
goni2409 <goni@intilab.com>
2026-04-06 14:20:48 +0700

    model kategori SK

--- app/Models/ParameterKategoriSk.php
+++ app/Models/ParameterKategoriSk.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Sector;
 
-class ParameterFdl extends Sector
+class ParameterKategoriSk extends Sector
 {
     protected $table = "parameter_kategori_sk";
     public $timestamps = false;
