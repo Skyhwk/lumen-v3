a051eec448a1f9ade03eba707fd2996ad43983f2
aliffadhil-intilab <aliffadhil@intilab.com>
2026-04-10 10:57:20 +0700

    update : rumus padatan terbaru

--- app/HelpersFormula/SpektroPadatanF.php
+++ app/HelpersFormula/SpektroPadatanF.php
@@ -5,10 +5,10 @@ namespace App\HelpersFormula;
 class SpektroPadatanF
 {
     public function index($data, $id_parameter, $mdl) {
-        $rumus = (($data->hp / 50) * (60/50)) * $data->fp;
+        $rumus = (($data->hp / 50) * (50/50)) * $data->fp;
         if(!is_null($mdl) && $rumus < $mdl) $rumus = '<' . $mdl;
         $rumus = str_replace(",", "", $rumus);
-        
+
         $data = [
             'hasil' => $rumus,
             'hasil_2' => '',
