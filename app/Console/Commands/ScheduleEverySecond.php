4b3963e739ccbead0e6ea7de3487935bfbfde1e3
Skyhwk <dedi@intilab.com>
2026-04-16 23:57:58 +0700

    release panic stress mysql

--- app/Console/Commands/ScheduleEverySecond.php
+++ app/Console/Commands/ScheduleEverySecond.php
@@ -1,49 +1,37 @@
 <?php
+
 namespace App\Console\Commands;
 
 use App\Helpers\WorkerAutomaticApprove;
-use App\Helpers\WorkerDailyQSDSales;
-use App\Helpers\WorkerReassign;
 use App\Helpers\WorkerSummaryParameter;
-use App\Helpers\WorkerSummaryQSD;
-use App\Helpers\WorkerUpdateKpiSales;
-use App\Helpers\WorkerFeeSales;
 use App\Services\EmailBlast;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class ScheduleEverySecond extends Command
 {
     protected $signature   = 'schedule:every-second';
-    protected $description = 'Run the scheduler every second (manual loop)';
+    protected $description = 'Run optimized scheduler every second';
 
     public function handle()
     {
         while (true) {
             try {
-                EmailBlast::sendEmailBlast();
-
-                // WorkerReassign::run();
-
-                // WorkerSummaryQSD::run();
-
-                // WorkerDailyQSDSales::run();
-
-                // WorkerApproveAnalyst::run();
-
-                WorkerSummaryParameter::run();
-                
-                // INI AUTO APPROVE
                 WorkerAutomaticApprove::run();
 
-                // WorkerUpdateKpiSales::run();
+                if (Carbon::now()->second % 5 === 0) {
+                    EmailBlast::sendEmailBlast();
+                }
 
-                // WorkerFeeSales::run();
+                if (Carbon::now()->second === 0) {
+                    WorkerSummaryParameter::run();
+                }
 
-                // Log::info('[ScheduleEverySecond] Loop berjalan pada: ' . date('Y-m-d H:i:s'));
             } catch (\Throwable $th) {
                 Log::error($th);
             }
+
             sleep(1);
         }
     }
