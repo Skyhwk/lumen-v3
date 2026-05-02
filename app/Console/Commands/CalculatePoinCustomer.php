818037924fc0488572692c34e808821064c44767
Skyhwk <dedi@intilab.com>
2026-05-02 10:43:54 +0700

    perbaikan dikit

--- app/Console/Commands/CalculatePoinCustomer.php
+++ app/Console/Commands/CalculatePoinCustomer.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Carbon\Carbon;
 
+use App\Models\SummaryInvoice;
 use App\Models\PointEarning;
 use App\Models\DailyQsd;
 use DB;
@@ -24,78 +25,66 @@ class CalculatePoinCustomer extends Command
         $this->info("  START CALCULATING POIN CUSTOMER  ");
         $this->info("  Time: " . Carbon::now()->toDateTimeString());
         // =======================START CALCULATING POIN CUSTOMER================================
-        $data = DailyQsd::where('is_lunas', 1)->where('is_point_calculated', 0)
+        // $data = DailyQsd::where('is_lunas', 1)->where('is_point_calculated', 0)
+        // ->orderBy('no_invoice')
+
+        $data = SummaryInvoice::whereIn('status_lunas', ['Lunas', 'Kelebihan Pembayaran'])
+        ->whereNotExists(function ($query) {
+            $query->select(DB::raw(1))
+                ->from('point_earnings')
+                ->whereColumn('point_earnings.source_id', 'summary_invoice.no_invoice')
+                ->where('point_earnings.source_type', 'summary_invoice');
+        })
+        ->whereDate('tgl_pelunasan', '>=', '2025-01-01')
         ->orderBy('no_invoice')
-        ->chunk(1000, function ($rows) {
+        ->chunk(500, function ($rows) {
 
             foreach ($rows as $item) {
         
-                DB::beginTransaction();
                 try {
-
-                    $daily = DailyQsd::where('uuid', $item->uuid)
-                        ->first();
         
-                    if (!$daily || $daily->is_point_calculated) {
-                        $this->error("  Daily QSD not found or already calculated: " . $item->uuid);
-                        DB::rollBack();
-                        continue;
-                    }
-        
-                    $exists = PointEarning::where('source_type', 'dailyqsd')
-                        ->where('source_id', $item->uuid)
+                    $exists = PointEarning::where('source_type', 'summary_invoice')
+                        ->where('source_id', $item->no_invoice)
                         ->exists();
         
                     if ($exists) {
-                        $this->error("  Point earning already exists: " . $item->uuid);
-                        $daily->update(['is_point_calculated' => 1]);
-                        DB::commit();
+                        $this->error("  Point earning already exists: " . $item->no_invoice);
                         continue;
                     }
         
-                    $poin = floor($item->revenue_invoice / $this->basicPoint);
+                    $poin = intval(floor($item->nilai_pelunasan / $this->basicPoint));
+        
                     if ($poin <= 0) {
-                        $this->error("  Poin is less than 0: " . $item->uuid);
-                        $daily->update(['is_point_calculated' => 1]);
-                        DB::commit();
+                        $this->error("  Poin is less than 0: " . $item->no_invoice);
                         continue;
                     }
         
-                    if (strpos($item->tanggal_pembayaran, ',') !== false) {
-                        $tglPembayaran = explode(',', $item->tanggal_pembayaran);
-                        $earnedAt = Carbon::parse(end($tglPembayaran));
-                    } else {
-                        $earnedAt = Carbon::parse($item->tanggal_pembayaran);
-                    }
+                    $earnedAt = Carbon::parse($item->tgl_pelunasan);
 
-                    if($earnedAt <= '2025-01-01') {
-                        $this->error("  Tanggal pembayaran is less than 2025-01-01: " . $item->pelanggan_ID);
-                        $daily->update(['is_point_calculated' => 1]);
-                        DB::commit();
+                    if ($earnedAt->lte(Carbon::parse('2025-01-01'))){
+                        $this->error("Tanggal pembayaran is less than 2025-01-01: " . $item->pelanggan_id . " - Total Poin: " . $poin . " - No Invoice: " . $item->no_invoice);
                         continue;
                     }
         
-                    PointEarning::create([
-                        'customer_id'       => $item->pelanggan_ID,
-                        'source_type'       => 'dailyqsd',
-                        'source_id'         => $item->uuid,
-                        'points'            => $poin,
-                        'claimed_points'    => 0,
-                        'expired_points'    => 0,
-                        'earned_at'         => $earnedAt,
-                        'claim_expired_at'  => $earnedAt->copy()->addYear(),
-                        'tier_expired_at'   => $earnedAt->copy()->addYears(2),
-                    ]);
-        
-                    $daily->update([
-                        'is_point_calculated' => 1
-                    ]);
-        
-                    DB::commit();
-                    $this->info("  Point earning created successfully: " . $item->pelanggan_ID);
+                    PointEarning::firstOrCreate(
+                        [
+                            'source_type' => 'summary_invoice',
+                            'source_id'   => $item->no_invoice,
+                        ],
+                        [
+                            'customer_id'       => $item->pelanggan_id,
+                            'points'            => $poin,
+                            'claimed_points'    => 0,
+                            'expired_points'    => 0,
+                            'earned_at'         => $earnedAt,
+                            'claim_expired_at'  => $earnedAt->copy()->addYear(),
+                            'tier_expired_at'   => $earnedAt->copy()->addYears(2),
+                        ]
+                    );
+
+                    $this->info("  Point earning created successfully: " . $item->pelanggan_id);
                     $this->info("  Points: " . $poin);
                 } catch (\Throwable $e) {
-                    DB::rollBack();
                     $this->error("  Error: " . $e->getMessage());
                     if (str_contains($e->getMessage(), 'Duplicate')) {
                         continue;
