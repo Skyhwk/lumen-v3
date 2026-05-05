830f283d0a61e543378dbc8ba68f32251d1deb6f
haroldintilab <harold@intilab.com>
2026-05-05 16:44:00 +0700

    update: monitoring LHP

--- app/Http/Controllers/api/MonitoringLhpController.php
+++ app/Http/Controllers/api/MonitoringLhpController.php
@@ -84,17 +84,6 @@ class MonitoringLhpController extends Controller
 
         /*
         |--------------------------------------------------------------------------
-        | Hanya yang sudah ada LHP rilis
-        |--------------------------------------------------------------------------
-        */
-
-        $rekapOrder->where(function ($q) {
-            $q->whereNotNull('link_lhp.jumlah_lhp_rilis')
-            ->where('link_lhp.jumlah_lhp_rilis', '>', 0);
-        });
-
-        /*
-        |--------------------------------------------------------------------------
         | FILTER tanggal_sampling / periode
         |--------------------------------------------------------------------------
         */
