ed88c527d1544460b0afd944556bd48d77b9fcb5
Skyhwk <dedi@intilab.com>
2026-05-19 00:47:19 +0700

    sar

--- app/Models/SarHeader.php
+++ app/Models/SarHeader.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Sector;
+use App\Models\QuotationNonKontrak;
 
 class SarHeader extends Sector
 {
@@ -13,7 +13,11 @@ class SarHeader extends Sector
     protected $guarded = [];
 
     public function detail(){
-        return $this->hasMany(SarDetail::class, 'id', 'id_header')
+        return $this->hasMany(SarDetail::class, 'id_header', 'id')
         ->where('is_active', true);
+    }
+
+    public function quotation(){
+        return $this->hasOne(QuotationNonKontrak::class, 'no_document', 'no_quotation');
     }
 }
