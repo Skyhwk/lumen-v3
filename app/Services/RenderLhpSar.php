0f2e72b9154055519ba998af41fe816cca1ec5d2
ranggamyd <ranggaamyd@gmail.com>
2026-05-18 22:36:02 +0700

    lhp sar

---
+++ app/Services/RenderLhpSar.php
@@ -0,0 +1,295 @@
+<?php
+
+namespace App\Services;
+
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
+
+class RenderLhpSar
+{
+    private $header;
+    private $detail;
+
+    private static $instance;
+
+    public static function setDataHeader($value)
+    {
+        if (!self::$instance) self::$instance = new self();
+
+        self::$instance->header = $value;
+
+        return self::$instance;
+    }
+
+    public static function setDataDetail($value)
+    {
+        if (!self::$instance) self::$instance = new self();
+
+        self::$instance->detail = $value;
+
+        return self::$instance;
+    }
+
+    public function render()
+    {
+        if (!$this->header) throw new \Exception("Header belum diset. Gunakan setHeader() sebelum render().");
+        if (!$this->detail) throw new \Exception("Detail belum diset. Gunakan setDetail() sebelum render().");
+
+        $filename = $this->execute($this->header, $this->detail);
+
+        self::$instance = null;
+
+        return $filename;
+    }
+
+    private function execute($header, $detail)
+    {
+        $defaultConfig = (new ConfigVariables())->getDefaults();
+        $defaultFontConfig = (new FontVariables())->getDefaults();
+
+        $mpdf = new MpdfService([
+            'mode' => 'utf-8',
+            'format' => 'A4',
+            'margin_header' => 12,
+            'margin_bottom' => 22,
+            'margin_footer' => 8,
+            'margin_top' => 27.5,
+            'margin_left' => 10,
+            'margin_right' => 10,
+            'orientation' => 'L',
+            'fontDir' => array_merge($defaultConfig['fontDir'], [__DIR__ . '/vendor/mpdf/mpdf/ttfonts']),
+            'fontdata' => $defaultFontConfig['fontdata'] + [
+                'roboto' => [
+                    'R'  => 'Roboto-Regular.ttf',
+                    'M'  => 'Roboto-Medium.ttf',
+                    'SB' => 'Roboto-SemiBold.ttf',
+                    'B'  => 'Roboto-Bold.ttf',
+                    'BI'  => 'Roboto-BoldItalic.ttf',
+                ]
+            ],
+        ]);
+
+        $stylesheet = $this->generateStylesheet();
+
+        $htmlHeader = view('TemplateLHP.LHPSAR.header', compact('header', 'detail'))->render();
+        $htmlBody = view('TemplateLHP.LHPSAR.left', compact('header', 'detail'))->render();
+        $htmlFooter = view('TemplateLHP.LHPSAR.footer', compact('header', 'detail'))->render();
+
+        $mpdf->SetHTMLHeader($htmlHeader);
+        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
+        $mpdf->WriteHTML($htmlBody);
+        $mpdf->SetHTMLFooter($htmlFooter);
+
+        $mpdf->showWatermarkImage = true;
+        $mpdf->SetWatermarkImage(public_path() . "/logo-watermark.png", -1, "", [110, 35]);
+
+        $mpdf->SetProtection(['print'], '', 'skyhwk12', 128);
+
+        $dir = public_path('dokumen/LHP-SAR/');
+        if (!file_exists($dir)) mkdir($dir, 0777, true);
+
+        $filename = 'LHP-' . str_replace("/", "-", $header->no_order) . '.pdf';
+
+        $mpdf->Output($dir . '/' . $filename, \Mpdf\Output\Destination::FILE);
+
+        return $filename;
+    }
+
+    private function generateStylesheet()
+    {
+        return "
+            .custom {
+                padding: 3px;
+                text-align: center;
+                border: 1px solid #000000;
+                font-weight: bold;
+                font-size: 9px;
+            }
+            .custom1 {
+                padding: 3px;
+                text-align: left;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #5b5b5b;
+                font-size: 9px;
+            }
+            .custom2 {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #5b5b5b;
+                font-size: 9px;
+            }
+            .custom3 {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .custom4 {
+                padding: 3px;
+                border: 1px solid #000000;
+                font-weight: bold;
+                font-size: 9px;
+            }
+            .custom5 {
+                text-align: left;
+            }
+            .custom6 {
+                padding: 3px;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+            }
+            .custom7 {
+                text-align: center;
+                border: 1px solid #000000;
+                font-weight: bold;
+                font-size: 9px;
+            }
+            .custom8 {
+                text-align: center;
+                font-style: italic;
+                border: 1px solid #000000;
+                font-weight: bold;
+                font-size: 9px;
+            }
+            .custom9 {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-3-dot {
+                padding: 3px;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #000000;
+                font-size: 9px;
+            }
+            .pd-3-solid {
+                padding: 3px;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-5-dot-center {
+                padding: 8px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #000000;
+                font-size: 9px;
+            }
+            .pd-3-dot-center {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #000000;
+                font-size: 9px;
+            }
+            .pd-5-dot-left {
+                padding: 8px;
+                text-align: left;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #000000;
+                font-size: 9px;
+            }
+            .pd-3-dot-left {
+                padding: 3px;
+                text-align: left;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px dotted #000000;
+                font-size: 9px;
+            }
+            .pd-5-solid-left {
+                padding: 8px;
+                text-align: left;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-3-solid-left {
+                padding: 3px;
+                text-align: left;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-5-solid-center {
+                padding: 8px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-3-solid-center {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                font-size: 9px;
+            }
+            .pd-5-solid-top-center {
+                padding: 8px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                font-size: 9px;
+                font-weight: bold;
+            }
+            .pd-3-solid-top-center {
+                padding: 3px;
+                text-align: center;
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                font-size: 9px;
+                font-weight: bold;
+            }
+            .bordered{
+                border-left: 1px solid #000000;
+                border-right: 1px solid #000000;
+                border-bottom: 1px solid #000000;
+                border-top: 1px solid #000000;
+                font-size: 9px;
+            }
+            .right {
+                float: right;
+                width: 40%;
+                height: 100%;
+            }
+            .left {
+                float: left;
+                width: 59%;
+            }
+            .leftMiddle{
+                float: left;
+                width: 50%;
+            }
+            .rightMiddle{
+                float: right;
+                width: 49%;
+            }
+            .left2 {
+                float: left;
+                width: 69%;
+            }
+        ";
+    }
+}
---
+++ resources/views/TemplateLHP/LHPSAR/footer.blade.php
@@ -0,0 +1,45 @@
+<table width="100%" style="font-size:7px;">
+    <tr>
+        <td width="18%" style="vertical-align: bottom; font-family: roboto; font-weight: bold;">
+            <div>PT Inti Surya Laboratorium</div>
+            <div>Ruko Icon Business Park Blok O No.5-6 BSD City, Jl. BSD Raya Utama, Cisauk, Sampora Kab. Tangerang
+                15341</div>
+            <div>021-5089-8988/89 contact@intilab.com</div>
+        </td>
+        <td width="60%"
+            style="vertical-align: bottom; text-align:center; padding-left:44px; margin:0; position:relative; min-height:100px; font-family: roboto; font-weight: bold;">
+            Laporan hasil pengujian ini hanya berlaku bagi sampel yang tercantum di atas. Lembar ini tidak boleh diubah
+            ataupun digandakan tanpa izin tertulis dari pihak Laboratorium.
+            <br>Halaman {PAGENO} - {nbpg}
+        </td>
+        <!-- signature -->
+        <td width="22%" style="position: relative; padding: 0; text-align: right;">
+
+            <table
+                style="position: absolute; bottom: 0; left: 0; text-align: center; font-family: Helvetica, sans-serif; font-size: 9px;"
+                width="260">
+                <tr>
+                    <td>{{ 'Tangerang, ' . \App\Helpers\Helper::tanggal_indonesia($header->tanggal_lhp) }}</td>
+                </tr>
+                <tr>
+                    <td style="height: 70px;"><img src="{{ public_path('qr_documents/' . $header->file_qr . '.svg') }}"
+                            width="50px" height="50px"></td>
+                </tr>
+                <tr>
+                    <td style="height: 70px;"></td>
+                </tr>
+                <tr>
+                    <td style="height: 10px;"></td>
+                </tr>
+            </table>
+            <table
+                style="position: absolute; bottom: 0; right: 0; font-family: Helvetica, sans-serif; font-size: 7px; text-align: right;">
+                <tr>
+                    <td style="font-family: roboto; font-weight: bold;">DP/7.8.1/ISL; Rev 3; 08 November 2022</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+    </tr>
+</table>
