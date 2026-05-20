153e990b3da550254deda62ea1fedffe075cf7fd
alawi-intilab <alawi@intilab.com>
2026-05-20 10:16:15 +0700

    remove comments

--- app/Models/DashboardComponent.php
+++ app/Models/DashboardComponent.php
@@ -8,12 +8,5 @@ use App\Models\Sector;
 class DashboardComponent extends Sector
 {
     protected $table = "dashboard_component";
-    // public $timestamps = false;
     protected $guarded = [];
-
-    // public function detail(){
-    //     return $this->belongsTo('App\Models\OrderDetail', 'no_sampel', 'no_sampel')
-    //     ->where('is_active', true);
-    // }
-    
 }
