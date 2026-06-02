f3a4666466b1982c6ba2debc5fb961f8d0076137
alawi-intilab <alawi@intilab.com>
2026-06-02 08:51:29 +0700

    add app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,7 +74,6 @@ foreach (glob(__DIR__ . '/../config/*.php') as $file) {
 
 $app->middleware([
     App\Http\Middleware\CorsMiddleware::class,
-    App\Http\Middleware\RateLimitMiddleware::class,
     App\Http\Middleware\Utf8Sanitizer::class
 ]);
 
@@ -82,11 +81,9 @@ $app->routeMiddleware([
     'auth.token' => App\Http\Middleware\CheckToken::class,
     'log.request' => App\Http\Middleware\LogRequest::class,
     'decrypt.slice' => \App\Http\Middleware\DecryptSliceMiddleware::class,
-    'rate.limit' => App\Http\Middleware\RateLimitMiddleware::class,
     'cors' => \App\Http\Middleware\CorsMiddleware::class,
     'auth.customer.token' => App\Http\Middleware\CheckCustomerToken::class,
     'director.auth.token' => App\Http\Middleware\directorApp\ApiTokenAuth::class,
-    'rate.limit.user' => App\Http\Middleware\UserRateLimitMiddleware::class,
 ]);
 
 /*
@@ -112,6 +109,8 @@ $app->register(Telegram\Bot\Laravel\TelegramServiceProvider::class);
 
 $app->register(Illuminate\Cache\CacheServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
+
+$app->register(Laravel\Tinker\TinkerServiceProvider::class);
 
 // Register facades
 $app->withFacades(true, [
