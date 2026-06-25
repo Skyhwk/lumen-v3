0d15bab9f5478508297803ba7e00e00127ca2fda
luthfi-intilab <luthfi@intilab>
2026-06-25 08:20:43 +0700

    update:penyesuaian rekapan piutang

--- app/Http/Controllers/api/RekapPiutangController.php
+++ app/Http/Controllers/api/RekapPiutangController.php
@@ -114,7 +114,10 @@ class RekapPiutangController extends Controller
                 SELECT SUM(oh.piutang) AS total_piutang_unbilled
                 FROM order_header AS oh
                 LEFT JOIN invoice AS inv ON oh.no_order = inv.no_order AND inv.is_active = 1
-                WHERE YEAR(oh.tanggal_order) = ? AND oh.is_active = 1 AND inv.id IS NULL
+                WHERE YEAR(oh.tanggal_order) = ? 
+                  AND oh.is_active = 1 
+                  AND oh.is_revisi = 0
+                  AND inv.id IS NULL
             ";
             $unbilledResult = DB::select($sqlUnbilled, [$year]);
             $unbilledTotal = $unbilledResult[0]->total_piutang_unbilled ?? 0;
