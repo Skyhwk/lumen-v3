94221ec776a82800149d3ece6e8b2a892d49683a
Skyhwk <dedi@intilab.com>
2026-07-02 14:42:27 +0700

    perbaikan

--- app/Http/Controllers/api/PurchaseOrdersController.php
+++ app/Http/Controllers/api/PurchaseOrdersController.php
@@ -516,7 +516,7 @@ class PurchaseOrdersController extends Controller
 
         $lineTotal = round($quantity * $unitPrice, 2);
         $subTotal = max(round($lineTotal - $discount, 2), 0);
-        $ppnAmount = round($lineTotal * ($ppnPercent / 100), 2);
+        $ppnAmount = round($subTotal * ($ppnPercent / 100), 2);
         $grandTotal = round($subTotal + $ppnAmount + $otherCost, 2);
 
         DB::beginTransaction();
