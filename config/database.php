9ca6b2cf77bc78fea095f0b7aaea3123f9b2a65c
Patah1309 <abdulpatah@intilab.com>
2026-07-20 09:50:13 +0700

    menambah connection lims

--- config/database.php
+++ config/database.php
@@ -152,6 +152,7 @@ return [
             'engine' => env('DB_ENGINE', null),
             'timezone' => env('DB_TIMEZONE', '+00:00'),
         ],
+        
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
