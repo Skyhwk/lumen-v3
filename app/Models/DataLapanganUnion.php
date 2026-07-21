b1ae7e8243acae32518b11c29ed89479ddfec046
alawi-intilab <alawi@intilab.com>
2026-07-21 18:33:33 +0700

    add coc page

--- app/Models/DataLapanganUnion.php
+++ app/Models/DataLapanganUnion.php
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Sector;
-use App\Models\DeviceIntilab;
 
 class DataLapanganUnion extends Sector
 {
     protected $connection = 'mysql';
+    public static $useLimsDetail = false;
     public $timestamps = false;
     protected $guarded = [];
 
@@ -18,22 +16,4 @@ class DataLapanganUnion extends Sector
         $mainDb = \DB::connection('mysql')->getDatabaseName();
         return $mainDb . '.data_lapangan_union';
     }
-=======
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Sector;
-use App\Models\DeviceIntilab;
-
-class DataLapanganUnion extends Sector
-{
-    public static $useLimsDetail = false;
-
-    protected $table = 'data_lapangan_union';
-    public $timestamps = false;
-
-    protected $guarded = [];
->>>>>>> 57e96a6a2074bd2c4dc9b9e19f7564320e789c4d
 }
