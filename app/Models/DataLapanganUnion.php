54a9a2e543ce7c3c200f37cde8e1d549b95a470e
Muhamad Abdul Patah <112842896+patah1309@users.noreply.github.com>
2026-07-21 10:05:00 +0700

    Refactor DataLapanganUnion model structure

--- app/Models/DataLapanganUnion.php
+++ app/Models/DataLapanganUnion.php
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Sector;
-use App\Models\DeviceIntilab;
-
-class DataLapanganUnion extends Sector
-{
-    protected $connection = 'mysql';
-    public $timestamps = false;
-    protected $guarded = [];
-
-    public function getTable()
-    {
-        $mainDb = \DB::connection('mysql')->getDatabaseName();
-        return $mainDb . '.data_lapangan_union';
-    }
-=======
 <?php
 
 namespace App\Models;
@@ -35,5 +14,4 @@ class DataLapanganUnion extends Sector
     public $timestamps = false;
 
     protected $guarded = [];
->>>>>>> 57e96a6a2074bd2c4dc9b9e19f7564320e789c4d
 }
