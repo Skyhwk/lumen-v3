f30d0d0772a5b0b4d9e74cea63c185f70a1e2fde
reyhan <reyhan@pop-os.localdomain>
2026-07-23 16:05:02 +0700

    fix not found pdf

--- app/Http/Controllers/api/LimsLhpUdaraUlkSinarUVController.php
+++ app/Http/Controllers/api/LimsLhpUdaraUlkSinarUVController.php
@@ -145,7 +145,7 @@ class LimsLhpUdaraUlkSinarUVController extends Controller
     public function previewLhp(Request $request)
     {
         try {
-            $header = LhpsSinarUvHeader::where('no_sampel', $request->no_sampel)->where('is_active', true)->first();
+            $header = LhpsSinarUvHeader::where('no_lhp', $request->no_lhp)->where('is_active', true)->first();
             if (!$header) {
                 return response()->json([
                     'status' => false,
@@ -167,24 +167,16 @@ class LimsLhpUdaraUlkSinarUVController extends Controller
                 }
             }
 
-            $fileName = \App\Services\LhpTemplate::setDataDetail($detail)
+            $pdfContent = \App\Services\LhpTemplate::setDataDetail($detail)
                 ->setDataHeader($header)
                 ->setDataCustom($groupedByPage)
                 ->whereView('DraftUlkSinarUv')
                 ->render('downloadLHPFinal', 'S');
 
-            // Find file
-            $filePath = base_path('public/dokumen/LHP_DOWNLOAD/' . $fileName);
-            if (!file_exists($filePath)) {
-                $filePath = base_path('public/dokumen/LHP/' . $fileName);
-            }
-
-            if (file_exists($filePath)) {
-                $base64 = base64_encode(file_get_contents($filePath));
-                return response()->json(['data' => $base64]);
-            }
-
-            return response()->json(['message' => 'Gagal merender PDF'], 404);
+            return response()->json([
+                'data' => base64_encode($pdfContent),
+                'message' => 'LHP berhasil dirender'
+            ], 200);
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
