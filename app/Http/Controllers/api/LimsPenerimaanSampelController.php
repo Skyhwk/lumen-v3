71945febd70253caa026b0135b7eb486ab249e33
reyhan <reyhan@pop-os.localdomain>
2026-07-24 17:56:45 +0700

    lims order detail

--- app/Http/Controllers/api/LimsPenerimaanSampelController.php
+++ app/Http/Controllers/api/LimsPenerimaanSampelController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
 use App\Http\Controllers\Controller;
+use App\Models\ScanSampelTc;
 use App\Models\Lims\OrderDetail;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -17,7 +18,23 @@ class LimsPenerimaanSampelController extends Controller
     {
         $date = Carbon::parse($request->date);
 
-        $data = OrderDetail::whereMonth('created_at', $date->month)
+        // Dapatkan semua no_sampel dari ScanSampelTc untuk bulan/tahun ini
+        $scanNoSampel = ScanSampelTc::whereMonth('created_at', $date->month)
+            ->whereYear('created_at', $date->year)
+            ->pluck('no_sampel')
+            ->toArray();
+
+        // Cari no_sampel yang juga ada di LIMS order_detail
+        $validNoSampels = [];
+        if (!empty($scanNoSampel)) {
+            $validNoSampels = OrderDetail::whereIn('no_sampel', $scanNoSampel)
+                ->pluck('no_sampel')
+                ->toArray();
+        }
+
+        // Query ScanSampelTc hanya untuk yang valid di LIMS
+        $data = ScanSampelTc::whereIn('no_sampel', $validNoSampels)
+            ->whereMonth('created_at', $date->month)
             ->whereYear('created_at', $date->year)
             ->orderBy('id', 'desc');
 
