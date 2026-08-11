e242be8d1fa9b8b11ebae66fc102d60e81f24fd4
goni2409 <goni@intilab.com>
2026-08-11 16:34:30 +0700

    update model order detail method microbio header untuk baca template stp 22 atau kategori swab test

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -432,7 +432,9 @@ class OrderDetail extends Sector
 
     public function swabOnMicrobio()
     {
-        return $this->belongsTo(MicrobioHeader::class, 'no_sampel', 'no_sampel')->where('microbio_header.parameter', 'like', "%Swab%")->where('is_active', true);
+        return $this->belongsTo(MicrobioHeader::class, 'no_sampel', 'no_sampel')->where(function ($query) {
+            $query->where('microbio_header.parameter', 'like', "%Swab%")->orWhere('microbio_header.template_stp', 22);
+        })->where('is_active', true);
     }
 
     public function ergonomiHeader()
