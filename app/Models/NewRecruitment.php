77f06ccddf0cc3326fd4f5b6e5b2584af933c36e
Patah1309 <abdulpatah@intilab.com>
2026-08-13 16:08:41 +0700

    Fixing

--- app/Models/NewRecruitment.php
+++ app/Models/NewRecruitment.php
@@ -65,6 +65,11 @@ class NewRecruitment extends Model
         return $this->hasOne(SallaryOffer::class, 'new_recruitment_id');
     }
 
+    public function salaryOffer()
+    {
+        return $this->hasOne(SallaryOffer::class, 'new_recruitment_id');
+    }
+
     public function candidateDataOffer()
     {
         return $this->hasOne(CandidateDataOffers::class, 'new_recruitment_id');
