e2a835e613ef35005242b8ffc9d5ca915b956f18
alawi-intilab <alawi@intilab.com>
2026-08-13 13:57:08 +0700

    add .

--- app/Http/Controllers/api/AtsInterviewUserController.php
+++ app/Http/Controllers/api/AtsInterviewUserController.php
@@ -600,7 +600,7 @@ class AtsInterviewUserController extends Controller
             ], 404);
         }
 
-        $targetEmail = 'alawi@intilab.com';
+        $targetEmail = 'abdulpatah@intilab.com';
         $user = $this->karyawan ?? $request->header('user') ?? 'HRD Admin';
 
         try {
