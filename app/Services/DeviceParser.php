e06209b0f224f03ab4aa12c9463d3d7d7e9bc9e3
Skyhwk <dedi@intilab.com>
2026-08-18 16:41:23 +0700

    perbaikan nama device

--- app/Services/DeviceParser.php
+++ app/Services/DeviceParser.php
@@ -13,44 +13,28 @@ class DeviceParser
             return 'Perangkat Lain';
         }
 
-        if (preg_match('/ipad/i', $userAgent)) {
-            return 'iPad (Tablet)';
-        }
-
-        if (preg_match('/iphone/i', $userAgent)) {
-            return 'iPhone';
-        }
-
-        if (preg_match('/android/i', $userAgent)) {
-            if (preg_match('/mobile/i', $userAgent)) {
-                return 'Android (HP)';
-            }
-
-            return 'Android (Tablet)';
-        }
-
-        if (preg_match('/windows phone/i', $userAgent)) {
-            return 'Windows Phone';
-        }
-
-        if (preg_match('/windows|win32/i', $userAgent)) {
-            return 'Windows (PC)';
-        }
-
-        if (preg_match('/macintosh|mac os x/i', $userAgent)) {
-            return 'Mac (PC)';
-        }
-
-        if (preg_match('/linux/i', $userAgent)) {
-            return 'Linux (PC)';
-        }
-
-        if (preg_match('/cros/i', $userAgent)) {
-            return 'Chromebook';
+        $superAppsLabel = self::parseSuperAppsLabel($userAgent);
+        if ($superAppsLabel !== null) {
+            return $superAppsLabel;
         }
 
         if (preg_match('/Thunder Client/i', $userAgent)) {
             return 'Thunder Client';
+        }
+
+        $os = self::detectOsLabel($userAgent);
+        $browser = self::detectBrowserLabel($userAgent);
+
+        if ($os && $browser) {
+            return $os . ' ' . $browser;
+        }
+
+        if ($os) {
+            return $os;
+        }
+
+        if ($browser) {
+            return $browser;
         }
 
         return 'Perangkat Lain';
@@ -67,5 +51,80 @@ class DeviceParser
             'ip_address' => $token->ip_address,
             'last_active_at' => $token->last_active_at,
         ];
+    }
+
+    private static function parseSuperAppsLabel(string $userAgent): ?string
+    {
+        if (preg_match('/intilab-apps\/([\d]+(?:\.[\d]+)*)/i', $userAgent, $matches)) {
+            return 'Aplikasi Super Apps ' . $matches[1];
+        }
+
+        if (preg_match('/Electron/i', $userAgent)) {
+            return 'Aplikasi Super Apps';
+        }
+
+        return null;
+    }
+
+    private static function detectOsLabel(string $userAgent): ?string
+    {
+        if (preg_match('/ipad/i', $userAgent)) {
+            return 'iPad';
+        }
+
+        if (preg_match('/iphone/i', $userAgent)) {
+            return 'iPhone';
+        }
+
+        if (preg_match('/android/i', $userAgent)) {
+            return preg_match('/mobile/i', $userAgent) ? 'Android' : 'Android Tablet';
+        }
+
+        if (preg_match('/windows phone/i', $userAgent)) {
+            return 'Windows Phone';
+        }
+
+        if (preg_match('/cros/i', $userAgent)) {
+            return 'ChromeOS';
+        }
+
+        if (preg_match('/windows nt|win32/i', $userAgent)) {
+            return 'Windows';
+        }
+
+        if (preg_match('/macintosh|mac os x/i', $userAgent)) {
+            return 'MacOS';
+        }
+
+        if (preg_match('/linux/i', $userAgent)) {
+            return 'Linux';
+        }
+
+        return null;
+    }
+
+    private static function detectBrowserLabel(string $userAgent): ?string
+    {
+        if (preg_match('/Edg\//i', $userAgent)) {
+            return 'Edge';
+        }
+
+        if (preg_match('/OPR\/|Opera/i', $userAgent)) {
+            return 'Opera';
+        }
+
+        if (preg_match('/Firefox\//i', $userAgent)) {
+            return 'Firefox';
+        }
+
+        if (preg_match('/Chrome\//i', $userAgent)) {
+            return 'Chrome';
+        }
+
+        if (preg_match('/Safari\//i', $userAgent)) {
+            return 'Safari';
+        }
+
+        return null;
     }
 }
