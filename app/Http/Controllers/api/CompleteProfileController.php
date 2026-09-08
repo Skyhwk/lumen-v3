28426c26696303c4122b1f3a12755e6bda261140
haroldintilab <harold@intilab.com>
2026-09-08 15:41:23 +0700

    update: complete profile

--- app/Http/Controllers/api/CompleteProfileController.php
+++ app/Http/Controllers/api/CompleteProfileController.php
@@ -231,6 +231,10 @@ class CompleteProfileController extends Controller
             'no_npwp' => 'Nomor NPWP',
             'no_bpjs_ks' => 'Nomor BPJS Kesehatan',
             'no_bpjs_tk' => 'Nomor BPJS Ketenagakerjaan',
+            'kode_pos_ktp' => 'Kode pos KTP',
+            'kode_pos_domisili' => 'Kode pos domisili',
+            'no_telepon_darurat' => 'Nomor telepon kontak darurat 1',
+            'no_telepon_darurat_2' => 'Nomor telepon kontak darurat 2',
         ] as $field => $label) {
             $value = trim((string) $request->input($field));
             if ($value !== '' && !preg_match('/^\d+$/', $value)) {
