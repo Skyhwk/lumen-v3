e2096be206f3cecbbf92ed53d736a7b6262a1f64
Skyhwk <dedi@intilab.com>
2026-09-14 17:27:09 +0700

    rekruitement

--- app/Http/Controllers/api/DataKandidatController.php
+++ app/Http/Controllers/api/DataKandidatController.php
@@ -36,64 +36,64 @@ class DataKandidatController extends Controller
     public function index(Request $request)
     {
         // dd($request->all());
-        // $searchYear = isset($request->search) ? date('Y', strtotime($request->search)) : date('Y');
+        $searchYear = isset($request->search) ? date('Y', strtotime($request->search)) : date('Y');
 
-        // $data = DataKandidat::select(
-        //     'cabang.*',
-        //     'posision.*',
-        //     'recruitment.*'
-        // )
-        //     ->leftJoin('master_jabatan as posision', 'recruitment.bagian_di_lamar', '=', 'posision.id')
-        //     ->leftJoin('master_cabang as cabang', 'recruitment.id_cabang', '=', 'cabang.id')
-        //     ->whereIn('recruitment.id_cabang', $this->privilageCabang)
-        //     ->where('recruitment.is_active', true)
-        //     ->where('recruitment.flag', 0)
-        //     ->where('recruitment.status', 'KANDIDAT')
-        //     // ->whereYear('recruitment.created_at', $searchYear)
-        //     ->distinct()
-        //     ->get();
+        $data = DataKandidat::select(
+            'cabang.*',
+            'posision.*',
+            'recruitment.*'
+        )
+            ->leftJoin('master_jabatan as posision', 'recruitment.bagian_di_lamar', '=', 'posision.id')
+            ->leftJoin('master_cabang as cabang', 'recruitment.id_cabang', '=', 'cabang.id')
+            ->whereIn('recruitment.id_cabang', $this->privilageCabang)
+            ->where('recruitment.is_active', true)
+            ->where('recruitment.flag', 0)
+            ->where('recruitment.status', 'KANDIDAT')
+            // ->whereYear('recruitment.created_at', $searchYear)
+            ->distinct()
+            ->get();
 
-        // $data = DataKandidat::with([
-        //     'cabang:id,nama_cabang',
-        //     'jabatan:id,nama_jabatan'
-        // ])
-        //     ->whereIn('id_cabang', $this->privilageCabang)
-        //     ->where('is_active', true)
-        //     ->where('flag', 0)
-        //     ->where('status', 'KANDIDAT')
-        //     ->whereYear('created_at', $request->year)
-        //     ->distinct();
+        $data = DataKandidat::with([
+            'cabang:id,nama_cabang',
+            'jabatan:id,nama_jabatan'
+        ])
+            ->whereIn('id_cabang', $this->privilageCabang)
+            ->where('is_active', true)
+            ->where('flag', 0)
+            ->where('status', 'KANDIDAT')
+            ->whereYear('created_at', $request->year)
+            ->distinct();
+
+        return Datatables::of($data)
+            ->filterColumn('cabang.nama_cabang', function ($query, $keyword) {
+                $query->whereHas('cabang', function ($q) use ($keyword) {
+                    $q->where('nama_cabang', 'like', '%' . $keyword . '%');
+                });
+            })
+            ->filterColumn('jabatan.nama_jabatan', function ($query, $keyword) {
+                $query->whereHas('jabatan', function ($q) use ($keyword) {
+                    $q->where('nama_jabatan', 'like', '%' . $keyword . '%');
+                });
+            })
+            ->filterColumn('nama_lengkap', function ($query, $keyword) {
+                $query->where('nama_lengkap', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('created_at', function ($query, $keyword) {
+                $query->whereDate('created_at', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('posisi_di_lamar', function ($query, $keyword) {
+                $query->where('posisi_di_lamar', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('tempat_lahir', function ($query, $keyword) {
+                $query->where('tempat_lahir', 'like', '%' . $keyword . '%');
+            })
+            ->filterColumn('umur', function ($query, $keyword) {
+                $query->where('umur', 'like', '%' . $keyword . '%');
+            })
+            ->make(true);
 
         // return Datatables::of($data)
-        //     ->filterColumn('cabang.nama_cabang', function ($query, $keyword) {
-        //         $query->whereHas('cabang', function ($q) use ($keyword) {
-        //             $q->where('nama_cabang', 'like', '%' . $keyword . '%');
-        //         });
-        //     })
-        //     ->filterColumn('jabatan.nama_jabatan', function ($query, $keyword) {
-        //         $query->whereHas('jabatan', function ($q) use ($keyword) {
-        //             $q->where('nama_jabatan', 'like', '%' . $keyword . '%');
-        //         });
-        //     })
-        //     ->filterColumn('nama_lengkap', function ($query, $keyword) {
-        //         $query->where('nama_lengkap', 'like', '%' . $keyword . '%');
-        //     })
-        //     ->filterColumn('created_at', function ($query, $keyword) {
-        //         $query->whereDate('created_at', 'like', '%' . $keyword . '%');
-        //     })
-        //     ->filterColumn('posisi_di_lamar', function ($query, $keyword) {
-        //         $query->where('posisi_di_lamar', 'like', '%' . $keyword . '%');
-        //     })
-        //     ->filterColumn('tempat_lahir', function ($query, $keyword) {
-        //         $query->where('tempat_lahir', 'like', '%' . $keyword . '%');
-        //     })
-        //     ->filterColumn('umur', function ($query, $keyword) {
-        //         $query->where('umur', 'like', '%' . $keyword . '%');
-        //     })
         //     ->make(true);
-
-        return Datatables::of([])
-            ->make(true);
     }
 
     public function approveKandidatApi(Request $request)
