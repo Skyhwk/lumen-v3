b7191ca3d2cc0ef7b91f917085f8f25d645bf8bd
Patah1309 <abdulpatah@intilab.com>
2026-09-25 10:03:20 +0700

    Fix jadwal services

--- app/Services/JadwalServices.php
+++ app/Services/JadwalServices.php
@@ -1081,9 +1081,9 @@ class JadwalServices
                         $updateQuery->where('no_quotation', $dataAdd->no_quotation);
                     }
 
-                    $trackingDates = $trackingDates->merge(
-                        (clone $updateQuery)->where('is_active', true)->pluck('tanggal')
-                    );
+                    // $trackingDates = $trackingDates->merge(
+                    //     (clone $updateQuery)->where('is_active', true)->pluck('tanggal')
+                    // );
                     $updateQuery->update(['is_active' => false]);
                 }
             }
